3a8a67f5ed70442eb51d69108aa2846c51b0c980
qiqakotyi <qiqakotyi@gmail.com>
2016-07-12 08:38:49 +0200

    images added , greensock animation on render..

--- public/js/controllers/users-controller.js
+++ public/js/controllers/users-controller.js
@@ -1,11 +1,13 @@
 (function (angular, undefined) {
     'use strict';
-    angular.module('users').controller('UsersCtrl', function ($scope, $timeout,usersService) {
+    angular.module('users').controller('UsersCtrl', function ($scope, $timeout, usersService) {
 
 
         //Functions
         //__________________________________________________________________________________________________________________
-
+        $scope.select = function (user) {
+            $scope.selected = user;
+        };
 
         //Events
         //__________________________________________________________________________________________________________________
@@ -14,10 +16,24 @@
         //Init
         //__________________________________________________________________________________________________________________
         usersService.getUsers().then(function (results) {
-               $scope.users = results;
+                $scope.users = results;
+                $scope.count = $scope.users.length;
             })
             .catch(function (err) {
             });
 
+    }).directive('onFinishRender', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    var item = angular.element('.item');
+                    $timeout(function () {
+                        TweenMax.staggerTo(item, 0.3, {opacity: 1, marginTop: 0, ease: Power2.easeOut}, 0.1);
+                    });
+                }
+
+            }
+        }
     });
 })(angular);
